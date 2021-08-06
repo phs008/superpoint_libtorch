b51499601a4686cd8c807b672f020ba959a2f921
Joshua Park <phs008@naver.com>
2021-08-06 15:17:38 +0900

    Delete download_weight_torch.sh
    do not need

--- download_weight_torch.sh
+++
@@ -1,4 +0,0 @@
-wget https://soongsilac-my.sharepoint.com/:u:/g/personal/phs008_soongsil_ac_kr/ERE4KfrXD3RBknZ9dFGcOYUBka5t5Kr1wf5SrRRp8mlSXg?e=xMqrE5
-cd ./Thirdparthy
-wget https://soongsilac-my.sharepoint.com/:u:/g/personal/phs008_soongsil_ac_kr/EdtuAaBaY6pFsjHjxM27YYIBeIov7aDFRI8Bx-D86lU9Ig?e=5a73oS
-wget https://soongsilac-my.sharepoint.com/:u:/g/personal/phs008_soongsil_ac_kr/EVWhU-_p2RZHsJKnVVjaWYQBO0Q-P3SydZesHypPyxXD0w?e=pVgnqN
